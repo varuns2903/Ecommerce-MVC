d9a795104253bde0df782249faee8cc8cbe722db
Varun S <varuns90150@gmail.com>
2025-04-02 11:03:12 +0530

    Connected to mongodb atlas

--- src/main/java/com/ecommerce/app/config/MongoConfig.java
+++ src/main/java/com/ecommerce/app/config/MongoConfig.java
@@ -1,19 +1,21 @@
 package com.ecommerce.app.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "com.yourpackage.repository")
 public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
 
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017");
+        return MongoClients.create(uri);
     }
 
     @Bean
